816767b2f4e5664f7c5bf73a9a18a827948fc8fb
Essien Effiom ESSIEN <esessien@EA.HOLCIM.NET>
2024-09-11 15:13:00 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App h-screen flex flex-col items-center gap-6 bg-[#14161b] text-white">
       <h1 className='text-xl md:text- lg:text-xl xl:text-6xl font-bold pt-36 mt-36 text-green-700'>Lafarge Africa PLC</h1>
-      <p className='text-xl md:text- lg:text-xl xl:text-4xl font-bold  text-blue-400'>A Member of Holcim</p>
+      {/* <p className='text-xl md:text- lg:text-xl xl:text-4xl font-bold text-blue-400'>A Member of Holcim</p> */}
       <button onClick={() => setShowModal(true)} className='bg-green-700 px-4 py-2 rounded-lg text-sm md:text-lg lg:text-xl'> Click to View</button>
      {showModal &&  <Modal onClose={() => setShowModal(false)} />}
     </div>
